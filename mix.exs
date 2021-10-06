0e6f3805e59f268d9b41907dad4a497200957253
kokolegorille <koko.le.gorille@gmail.com>
2021-10-07 00:53:26 +0200

    Bump to 1.6

--- mix.exs
+++ mix.exs
@@ -36,13 +36,13 @@ defmodule Api.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.9"},
-      {:phoenix_ecto, "~> 4.2"},
-      {:ecto_sql, "~> 3.6"},
+      {:phoenix, "~> 1.6.0"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
       # {:phoenix_live_dashboard, "~> 0.4.0"},
       {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 0.5"},
+      {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.4"},
